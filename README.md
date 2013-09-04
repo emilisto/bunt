ce30d7e19f1e4f1c4195033065d398acae360456
Emil Stenqvist <emsten@gmail.com>
2013-09-04 12:55:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Bunt - a minimalist bundling method for client-side JavaScript
+
+This is a pre-alpha project for bringing some peace, sanity and
+pragmatism to client-side JavaScript. It's right in the same playing
+field as projects like component, grunt, yeoman, bower, browserify, etc.
+
+After observing the development of these tools and listened to the
+colorful and ongoing debate, I've come to believe in the following
+principles:
+
+1. **Reproducability** - a module that can be built and used at one
+point in time must never be broken or has its behavior changed because
+its dependencies change.
+2. **Encapsulating nested dependencies**. Multiple versions of the same
+dependency - even if not desired - must be able to co-exist in a single
+bundle. This is the only way of solving certain dependency problems.
+3. **Minimal restrictions** Allow for all kinds of compile-to-JS
+languages, pre-processed CSS, etc. For each requirement that is enforced
+by a bundling system, a certain part of the developer community is
+excluded. This is undesirable. 
+4. **Minimal external surface** Similarly to 3., using the bundled
+components must impose very little on the user and his/her site. It
+should be as simple as including one JavaScript file and a stylesheet.
+
+To create a system that conforms to these principles, one needs
+
+1. a package manager
+2. a tool that bundles JavaScript
+3. something that ties 1. and 2. together
+
+As I believe in inventing as little as possible, Bunt solves only
+3. and uses npm for package management and browserify for bundling.
+
+
+## Todo
+
+- Flesh out this document in more detail
---
+++ index.js
@@ -0,0 +1,162 @@
+var exec = require('child_process').exec;
+var path = require('path');
+var fs = require('fs.extra');
+var util = require("util")
+
+var through = require('through');
+var browserify = require('browserify');
+var async = require('async');
+var concat = require('concat-stream');
+var CombinedStream = require('combined-stream');
+var glob = require('glob');
+
+var findPackage = require('./lib/pkgfinder');
+
+function findDependencies(entry) {
+  var seen = {};
+  var b = browserify();
+  b.add(path.resolve(entry));
+  var depstream = b.deps({});
+
+  return depstream.pipe(through(function(dep) {
+      var out = this;
+
+      findPackage(path.dirname(dep.id), function(err, pkg, pkgfile) {
+        if(err) return out.emit('error', err);
+
+        if(!pkgfile.bunt) return;
+        if(seen[pkg]) return;
+
+        seen[pkg] = true;
+        out.queue({ pkg: pkg, pkgfile: pkgfile });
+      });
+    }))
+};
+
+function build(entry, opts) {
+
+  opts = opts || {};
+
+  var buildDir = path.resolve('build');
+
+  findPackage(path.resolve(entry), function(err, _pkg, _pkgfile) {
+
+    var rootPackage = _pkgfile;
+
+    console.log('Building %s\n', rootPackage.name);
+
+    async.waterfall([
+
+      function cleanBuildDirectory(cb) {
+        console.log('  - cleaning build directory');
+        fs.rmrf(buildDir, function() {
+          fs.mkdir(buildDir, cb);
+        });
+      },
+
+      function buildDependencies(cb) {
+        findDependencies(entry).pipe(concat(function(deps) {
+          var tasks = deps.map(function(dep) {
+            return buildOne.bind(null, dep);
+          });
+          async.parallel(tasks, cb);
+        }))
+      },
+
+      function concatenateCss(_, cb) {
+        console.log('  - concatenating stylesheets');
+
+        // options is optional
+        glob(path.join(buildDir, "**/*.css"), function (er, files) {
+          var combinedStream = CombinedStream.create();
+          combinedStream.append('/* Styles concatenated using bunt */\n');
+
+          // FIXME: preserve order of concatenated stylesheets files - the
+          // top-level ones should go last, so they override nested ones.
+          files.forEach(function(file) {
+            var name = path.basename(path.dirname(file));
+            if(name === 'build') name = rootPackage.name;
+
+            combinedStream.append(util.format('\n/* BEGIN styles for "%s" */\n', name));
+            combinedStream.append(fs.createReadStream(file));
+            combinedStream.append(util.format('\n/* END styles for "%s" */\n', name));
+          });
+
+          combinedStream.pipe(fs.createWriteStream(path.join(buildDir, '_style.css')));
+          combinedStream.on('end', function(err) {
+            if(err) return cb(err);
+            fs.rename(
+              path.join(buildDir, '_style.css'),
+              path.join(buildDir, 'style.css'),
+            cb);
+          });
+        })
+      },
+
+      function buildIndex(cb) {
+        var out = fs.createWriteStream('build/index.js');
+        entry = path.resolve(entry);
+        var b = browserify();
+        b.transform('brfs');
+        b.add(entry);
+
+        var bundle = b.bundle({
+          debug: !!opts.debug,
+          standalone: opts['global-name'] || rootPackage.name
+        });
+        bundle
+          .on('error', cb)
+          .on('end', cb)
+          .pipe(out);
+      },
+      function done() {
+        // TODO: print total bundle size and list of assets
+        console.log('\nAll done! Bundled bunt and dependencies are in ./%s',
+            path.relative(process.cwd(), buildDir));
+      }
+
+    ]);
+
+  });
+
+
+  function buildOne(bunt, cb) {
+    var dir = path.dirname(bunt.pkg),
+        pkgname = bunt.pkgfile.name;
+
+    console.log('  - building assets for "%s" in %s', pkgname, dir);
+
+    async.series([
+      function prepublish(cb) {
+        var child = exec('npm run-script prepublish', {
+          cwd: dir
+        }, function (error, stdout, stderr) {
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+          cb();
+        });
+      },
+      function copyAssets() {
+        var dst = path.join(buildDir, pkgname),
+            pkgBuildDir = path.join(path.dirname(bunt.pkg), 'build');
+
+        if(pkgBuildDir === buildDir) return cb();
+
+        fs.copyRecursive(pkgBuildDir, dst, cb);
+      }
+    ]);
+
+
+    // TODO:
+    // 1. create a directory A in the build/ directory for the bunt
+    // 2. run `npm prepublish` and copy all resulting assets to the directory
+    // created in 1.
+  }
+
+};
+
+module.exports = {
+  findDependencies: findDependencies,
+  build: build
+};
